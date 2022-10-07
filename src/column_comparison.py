42d6d0da340964243e1c597dafadae00b51b9ec0
casey <cpmoden@gmail.com>
2022-10-07 13:03:17 -0400

    add exemplar array

--- src/column_comparison.py
+++ src/column_comparison.py
@@ -3,10 +3,14 @@
 import numpy as np
 
 def column_comparison(a):
-
+    pass
     
 def main():
-    pass
+    a = np.array([[8,9,3,8,8],
+                  [0,5,3,9,9],
+                  [5,7,6,0,4],
+                  [7,8,1,6,2],
+                  [2,1,3,5,8]])
 
 if __name__ == "__main__":
     main()
